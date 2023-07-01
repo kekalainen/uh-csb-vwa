1c4b582db25758c87aaf6464dfa1200958dd7704
Kekalainen <git@kekalainen.me>
2023-07-01 19:35:30 +0000

    Refactor auth check, prevent re-registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
+from .mixins import UserIsAnonymousMixin
 
-class RegisterView(CreateView):
+
+class RegisterView(UserIsAnonymousMixin, CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("accounts:login")
     template_name = "registration/register.html"
 
 
-class LoginAnonymousView(UserPassesTestMixin, LoginView):
-    def test_func(self):
-        return self.request.user.is_anonymous
+class LoginAnonymousView(UserIsAnonymousMixin, LoginView):
+    pass
 
 
 class LogoutPostView(LogoutView):
