476178511bc583e4a7a94d61d60bfe043958b7e7
Kekalainen <git@kekalainen.me>
2023-07-01 19:21:31 +0000

    Implement register view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import LoginAnonymousView, LogoutPostView
+from .views import RegisterView, LoginAnonymousView, LogoutPostView
 
 urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginAnonymousView.as_view(), name="login"),
     path("logout/", LogoutPostView.as_view(), name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/register.html"
+
 
 class LoginAnonymousView(UserPassesTestMixin, LoginView):
     def test_func(self):
