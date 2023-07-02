b0d9cb69a953537e1fc220dbfdd56e7279c78d01
Kekalainen <git@kekalainen.me>
2023-07-02 13:27:54 +0000

    Implement post create view & form

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import PostListView
+from .views import PostCreateView, PostListView
 
 app_name = "posts"
 
 urlpatterns = [
     path("", PostListView.as_view(), name="list"),
+    path("create/", PostCreateView.as_view(), name="create"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,30 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 
 from .models import Post
 
 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ("content",)
+    http_method_names = ["post", "options"]
+    success_url = reverse_lazy("posts:list")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 class PostListView(ListView):
     model = Post
     paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        create_view = PostCreateView()
+        context["form"] = create_view.get_form_class()
+
+        return context
