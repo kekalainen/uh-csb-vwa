4effe50efd0f8aa3c6e97af905fbd6d3804e486e
Kekalainen <git@kekalainen.me>
2023-07-03 14:29:46 +0000

    Add object ownership mixin

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -4,3 +4,13 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 class UserIsAnonymousMixin(UserPassesTestMixin):
     def test_func(self):
         return self.request.user.is_anonymous
+
+
+class UserOwnsObjectMixin(UserPassesTestMixin):
+    object_owner_field_name = "owner"
+
+    def test_func(self):
+        obj = self.get_object()
+        obj_owner = getattr(obj, self.object_owner_field_name)
+
+        return self.request.user == obj_owner
