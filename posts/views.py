437e3b706b6fb3dc5a823b1e73bc409ad0c7f6b0
Kekalainen <git@kekalainen.me>
2023-07-03 09:50:36 +0000

    Implement post search

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.validators import MaxLengthValidator
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
@@ -29,3 +30,19 @@ class PostListView(ListView):
         context["form"] = create_view.get_form_class()
 
         return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filter_keyword = self.request.GET.get('filter')
+
+        if filter_keyword:
+            validator = MaxLengthValidator(255)
+            validator(filter_keyword)
+
+            # String interpolation (vulnerable to blind SQLi):
+            queryset = queryset.extra(where=["content LIKE '%%%s%%'" % filter_keyword])
+
+            # Parameterized query (safe):
+            # queryset = queryset.filter(content__icontains=filter_keyword)
+
+        return queryset
